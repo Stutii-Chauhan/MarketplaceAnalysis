dbdfa214f1b43435fa3412e00b20724270441d62
Stutii-Chauhan <stutiichauhan@gmail.com>
2025-05-13 18:21:41 +0530

    Update 2_Best_Sellers.py

--- pages/2_Best_Sellers.py
+++ pages/2_Best_Sellers.py
@@ -148,7 +148,11 @@ def render_best_sellers(gender):
                                         <b>Brand:</b> {row.get('brand', 'N/A')}<br>
                                         <b>Model:</b> {row.get('model_number', 'N/A')}<br>
                                         <b>Price:</b> ₹{int(row['price'])}<br>
-                                        <b>Rating:</b> {round(row['rating(out_of_5)'], 1) if pd.notna(row['rating(out_of_5)']) else "N/A"}/5<br>
+                                        <b>Rating:</b> {
+                                            f"{round(float(row['rating(out_of_5)']), 1)}"
+                                            if pd.notna(row['rating(out_of_5)']) and str(row['rating(out_of_5)']).replace('.', '', 1).isdigit()
+                                            else "N/A"
+                                        }/5<br>
                                         <b>Discount:</b> {
                                             "No" if pd.notna(row["discount"]) and row["discount"] in ["0", "0.0"]
                                             else row["discount"] if pd.notna(row["discount"])
