f3e378e443c44c458f1b72fb67047f62e79366da
Stutii-Chauhan <stutiichauhan@gmail.com>
2025-05-14 21:05:22 +0530

    Update cleaning_bestsellers_women.py

--- cleaning_bestsellers_women.py
+++ cleaning_bestsellers_women.py
@@ -378,4 +378,4 @@ df['file'] = df['file'].astype(int)
 df = df.sort_values(by='file')
 
 #Upload to Supabase
-df.to_sql("ratings", con=engine, if_exists="replace", index=False)
+df.to_sql("ratingcheck", con=engine, if_exists="replace", index=False)
